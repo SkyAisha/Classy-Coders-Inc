6fa3e8961ed2b815702d48cec31695ed108fa32c
SkyAisha <aisha.adia@sky.uk>
2023-10-25 14:41:19 +0100

    all tests passed

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,10 +1,23 @@
-class Manager {
+const { Employees } = require("./Employees");
+class Manager extends Employees {
+  #employeesManaged;
+  constructor(name, position, salary, department) {
+    super(name, position, salary);
+    this.department = department;
+    this.#employeesManaged = [];
+  }
 
+  getEmployeesManaged() {
+    return this.#employeesManaged;
+  }
 
-
-    
+  setEmployeesManaged(employee) {
+    if (employee instanceof Employees) {
+      this.#employeesManaged.push(employee);
+    }
+  }
 }
 
 module.exports = {
-    Manager,
-}
+  Manager,
+};
--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,21 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
+class SalesPerson extends Employees {
+  #totalsales;
+  constructor(name, position, salary) {
+    super(name, position, salary);
+    this.clients = [];
+    this.#totalsales = 0;
+  }
 
+  getSalesNumber() {
+    return this.#totalsales;
+  }
 
-
-    
+  makeSale(amount) {
+    this.#totalsales += amount;
+  }
 }
 
 module.exports = {
-    SalesPerson,
-}
+  SalesPerson,
+};
